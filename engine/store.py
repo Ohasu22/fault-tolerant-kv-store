31eb7c8adb2366378db137246ca067aee6daf4af
Ohasu22 <ojasgharde22@gmail.com>
2026-02-21 03:45:22 +0530

    Phase 2: Added the skeleton for integration of snapshot in store

--- engine/store.py
+++ engine/store.py
@@ -7,14 +7,20 @@ from engine.ttl import TTLManager
 from engine.wal import WriteAheadLog
 from engine.eviction import LRUCache
 
+#edit 21/02/26: adding my snapshot here
+from engine.snapshot import SnapshotManager
+
 class KeyValueStore:
     #capacity from LRUCache, for path I'll use the same variable I used in recovery
-    def __init__(self, capacity, wal_path):
+    def __init__(self, capacity, wal_path, snapshot_path = None):
         self.capacity = capacity
         self.cache = {} #key : (value, expiry_timestamp)
         self.ttl = TTLManager()
         self.lru = LRUCache(capacity)
         self.wal = WriteAheadLog(wal_path)
+
+        # edit
+        self.snapshot = SnapshotManager(snapshot_path) if snapshot_path else None
 
     #not the same put as my eviction
     def put(self, key, value, ttl = None):
@@ -70,6 +76,15 @@ class KeyValueStore:
 
     def close(self):
         self.wal.close()
+
+    #edit 21/02/25
+    def create_snapshot(self):
+        #just needs to save the snapshot if it exists
+        pass
+
+    def recover(self):
+        #will read from the snapshot_path file and redo the put_internal function
+        pass
 
 
 
